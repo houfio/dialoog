cbef47c443026278566432d04c4dda343603be5a
houfio <26674115+houfio@users.noreply.github.com>
2020-11-19 23:31:08 +0100

    Update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import filesize from 'rollup-plugin-filesize';
 import typescript from 'rollup-plugin-typescript2';
 import resolve from 'rollup-plugin-node-resolve';
 
-import { main, module, dependencies, devDependencies } from './package.json';
+import { main, module, dependencies, peerDependencies, devDependencies } from './package.json';
 
 export default {
   input: join(__dirname, 'src', 'index.ts'),
@@ -22,6 +22,7 @@ export default {
   ],
   external: [
     ...Object.keys(dependencies),
+    ...Object.keys(peerDependencies),
     ...Object.keys(devDependencies)
   ],
   plugins: [
